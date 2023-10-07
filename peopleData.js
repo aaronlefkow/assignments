ae534d78e1af18b5d06a0197735282b165e5f663
Aaron <20589499+aaronlefkow@users.noreply.github.com>
2023-10-06 21:45:48 -0500

    people working with PTO and different availability

--- peopleData.js
+++ peopleData.js
@@ -1,3 +1,16 @@
+const differentAvailabilityData = {
+    "John": {
+        Monday: { startTime: "8:00 AM", endTime: "4:00 PM" }
+    }
+};
+
+const ptoData = {
+    "John": {
+        Wednesday: true // John is on PTO on Wednesday
+    },
+};
+
+
 const peopleData = {
     "John": {
         team: "Blue",
@@ -7,12 +20,8 @@ const peopleData = {
             Wednesday: { startTime: "08:00 AM", endTime: "05:00 PM" },
             Thursday: { startTime: "08:00 AM", endTime: "05:00 PM" },
         },
-        differentAvailability: {
-            Monday: { startTime: "8:00 AM", endTime: "4:00 PM" }
-        },
-        pto: {
-            Wednesday: true, // John is on PTO on Wednesday
-        },
+        differentAvailability: differentAvailabilityData["John"],
+        pto: ptoData["John"],
         trainingStatus: {
             Surgery: "Trained",
             HTMI: "In Training",
@@ -29,9 +38,8 @@ const peopleData = {
             Thursday: { startTime: "08:00 AM", endTime: "05:00 PM" },
             Friday: { startTime: "08:00 AM", endTime: "05:00 PM" },
         },
-        pto: {
-
-        },
+        differentAvailability: differentAvailabilityData["Aaron"],
+        pto: ptoData["Aaron"],
         trainingStatus: {
             Surgery: "Trained",
             HTMI: "In Training",
